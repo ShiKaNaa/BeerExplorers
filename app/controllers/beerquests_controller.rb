c2db17a219f3ec4338533ee322ab1128da42be79
Raphael HOUDRY <raphael-59200@hotmail.fr>
2022-06-14 21:30:58 +0200

    controllers for app

---
+++ app/controllers/beerquests_controller.rb
@@ -0,0 +1,2 @@
+class BeerquestsController < ApplicationController
+end
---
+++ test/controllers/beerquests_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class BeerquestsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
