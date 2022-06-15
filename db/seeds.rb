8c38666fa7f1bb3cadbc91f64395b7bf4766543e
Raphael HOUDRY <raphael-59200@hotmail.fr>
2022-06-15 16:55:37 +0200

    seed de bière ajoutés

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+puts "Destroying all beers"
+Beer.destroy_all
+
+puts "Creating 50 beers"
+50.times do
+  Beer.create!(name: Faker::Beer.name, abv: Faker::Beer.alcohol, ibu: Faker::Beer.ibu, style: Faker::Beer.style, photo: Faker::LoremFlickr.image(size: "150x150", search_terms: ['beer']),country: Faker::Address.country, hops: Faker::Beer.hop)
+end
+puts "I'm done"
