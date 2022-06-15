c234058fffc9446394cb41c62ffca2a4e862c4d2
Raphael HOUDRY <raphael-59200@hotmail.fr>
2022-06-15 16:59:43 +0200

    2 user seed ajouté

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,4 +15,11 @@ puts "Creating 50 beers"
 50.times do
   Beer.create!(name: Faker::Beer.name, abv: Faker::Beer.alcohol, ibu: Faker::Beer.ibu, style: Faker::Beer.style, photo: Faker::LoremFlickr.image(size: "150x150", search_terms: ['beer']),country: Faker::Address.country, hops: Faker::Beer.hop)
 end
+
+puts "Destroying all users"
+User.destroy_all
+
+puts "Creating 2 users"
+User.create!(email: "test101@gmail.com", nickname: "Shika", password: "123456")
+User.create!(email: "test202@gmail.com", nickname: "Sorbet", password: "123456")
 puts "I'm done"
