62983ee818914c4538bc88b87a1947533bbf0549
Raphael HOUDRY <raphael-59200@hotmail.fr>
2022-06-16 23:31:44 +0200

    working on beerquest form

--- app/controllers/beerquests_controller.rb
+++ app/controllers/beerquests_controller.rb
@@ -1,5 +1,22 @@
 class BeerquestsController < ApplicationController
   def index
+    @beerquests = Beerquest.all
+    @beerquest_new = Beerquest.new
+  end
 
+  def create
+    @user_id = current_user
+    @beerquest = Beerquest.new(beerquest_params)
+    @beerquest.save
+    redirect_to beerquests_path
+    # else
+    #   render :index
+    # end
+  end
+
+  private
+
+  def beerquest_params
+    params.require(:beerquest).permit(:name, :beer_id, @user_id)
   end
 end
--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,2 +1,7 @@
 class Beer < ApplicationRecord
+  has_many :beer_collection_of_bars
+  has_many :recommendations
+  has_many :ratings
+  has_many :chatrooms
+  has_many :beerquests
 end
